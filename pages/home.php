4fb148051d7ea4e12f1303694562703c155beb34
iqbal farhan syuhada <iqbalfarhan1996@gmail.com>
2023-12-29 19:32:04 +0800

    update halaman home

--- pages/home.php
+++ pages/home.php
@@ -2,3 +2,16 @@
 <p>Latar belakang Hotel adalah salah satu cabang pariwisata yang terus berkembang di seluruh dunia. Di bawah naungan latar belakang ini, para penyedia hotel mengalokasikan waktu, tenaga, dan sumber daya lainnya untuk menciptakan tempat menginap yang nyaman, aman, dan berkelas internasional.</p>
 <p>Sebagai penyedia hotel, kita berusaha untuk memberikan pengalaman menginap yang lebih baik bagi para tamu. Untuk mencapai hal ini, kita menginvestasikan pada aspek-aspek seperti kualitas layanan, fasilitas, dan infrastruktur hotel. Selain itu, kita juga mengupayakan hubungan yang baik dengan komunitas sekitar untuk mendukung peningkatan kualitas hidup warga setempat.</p>
 <p>Dalam berkembangnya hotel, kita terus mengembangkan dan memperbarui tampilan dan pelayanan kami. Dengan mengevaluasi permintaan pasar dan mengadopsi teknologi terbaru, kita berusaha untuk menciptakan hotel yang lebih inovatif dan relevan bagi kebutuhan tamu saat ini.</p>
+
+
+kelompok 1: web hotel
+1. rizal
+2. ilyas
+3. oswald
+4. ajeng
+
+kelompok 2: web restorant
+1. richard
+2. aliyosa
+3. khoirun nugraha
+4. naila
